b0eb64af3dc8d9c837a655b9e35bb3af0786c912
Pathernaxx <kresnik13@gmail.com>
2015-07-21 14:40:27 +0900

    member

--- src/main/java/com/teamplanner/mapper/MemberMapper.java
+++ src/main/java/com/teamplanner/mapper/MemberMapper.java
@@ -1,5 +1,13 @@
 package com.teamplanner.mapper;
 
-public class MemberMapper {
+import java.util.HashMap;
 
+import com.teamplanner.dto.Member;
+
+public interface MemberMapper {
+	void insertMember(Member member);
+	boolean selectMemberByIdCheck(String id);
+	Member selectMemberByMemberNo(int memberNo);
+	void updateMember(Member member);
+	Member selectMemberByIdAndPassword(HashMap<String, String> params);
 }
