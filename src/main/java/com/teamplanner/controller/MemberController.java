1bde77040025fed8858cd99b2ac0a9bbe80189ce
ravakwang <ravakwang@hotmail.com>
2015-07-24 10:21:17 +0900

    DDD

--- src/main/java/com/teamplanner/controller/MemberController.java
+++ src/main/java/com/teamplanner/controller/MemberController.java
@@ -1,23 +1,44 @@
 package com.teamplanner.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.teamplanner.commons.Util;
+import com.teamplanner.dto.Member;
 import com.teamplanner.repository.MemberRepository;
 
 @Controller
 @RequestMapping(value="member")
 public class MemberController {
 	
+	private MemberRepository memberRepository;
+	@Autowired
+	@Qualifier("memberRepository")
+	public void setMemberDao(MemberRepository memberRepository){
+		this.memberRepository=memberRepository;
+	}
 
-	
-	@RequestMapping(value="member.action", method = RequestMethod.GET)
-	public String MemberLink(){
+	//member 회원정보 수정 DB적용
+		@RequestMapping(value="memberUpdate", method = RequestMethod.POST)
+		public String memberUpdate(Member member, HttpSession session){
+			
+			String pw = member.getPassword();
+			pw=Util.getHashedString(pw, "SHA-1");
+			
+			memberRepository.updateMember(member);
+			
+			int no = member.getNo();
+			memberRepository.updateMember(member);
+			session.setAttribute("loginuser", member);
+			
+			return "redirect:/member/memberView.action?memberNo="+no;
+		}
 		
-		return "member/memberedit";
 	}
 	
-}
+
--- src/main/java/com/teamplanner/repository/MemberRepository.java
+++ src/main/java/com/teamplanner/repository/MemberRepository.java
@@ -4,9 +4,14 @@ import com.teamplanner.dto.Member;
 
 public interface MemberRepository {
 	void insertMember(Member member);
+
 	String selectMemberByUserNameCheck(String userName);
+
 	String selectMemberByEmailCheck(String email);
+
 	Member selectMemberByMemberNo(int memberNo);
+
 	void updateMember(Member member);
+
 	Member selectMemberByIdAndPassword(String email, String password);
 }
