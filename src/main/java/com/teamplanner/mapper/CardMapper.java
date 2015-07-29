67da7fab17f8a4e27af34f5632217b0c4c1cb2a7
yoojung <yoojung1224@naver.com>
2015-07-29 14:15:20 +0900

    yj

--- src/main/java/com/teamplanner/mapper/CardMapper.java
+++ src/main/java/com/teamplanner/mapper/CardMapper.java
@@ -17,4 +17,6 @@ public interface CardMapper {
 	List<CheckItem> selectCheckItem(int checklistNo);
 	List<TagMember> selectTagMember(int cardNo);
 	
+	void insertAttachment(int cardNo, int boardNo);
+	
 }
